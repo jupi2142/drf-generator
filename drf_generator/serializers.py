728f116a409dba0ef6c9f0051f54176dd12cbc60
Henock Tesfaye <henock.tesfaye@ahadoo.com>
2020-08-10 13:48:40 +0300

    Version bump, and comment removal

--- drf_generator/serializers.py
+++ drf_generator/serializers.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Default serializer setting, they can specify their own serializer and
-# view[set] class
-# A separate serializer maker which also accepts mixins
-# A separate viewset maker which also accepts mixins
-# Expand?
 from .settings import SERIALIZER
 
 
